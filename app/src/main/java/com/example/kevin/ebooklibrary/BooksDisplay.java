f002bc4722e585e3ecef5cd83c5a10c5824cf115
Kminttech <kminttech@gmail.com>
2018-04-17 13:47:58 -0500

    Added Java Files for Layouts

---
+++ app/src/main/java/com/example/kevin/ebooklibrary/BooksDisplay.java
@@ -0,0 +1,13 @@
+package com.example.kevin.ebooklibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BooksDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books);
+    }
+}
