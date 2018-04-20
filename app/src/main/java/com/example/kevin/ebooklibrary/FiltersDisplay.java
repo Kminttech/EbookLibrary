10e7da5886a529a8d74c3291fe2753310993961c
Kminttech <kminttech@gmail.com>
2018-04-20 15:11:30 -0500

    Basic fetching of passed data and layout modifications

--- app/src/main/java/com/example/kevin/ebooklibrary/FiltersDisplay.java
+++ app/src/main/java/com/example/kevin/ebooklibrary/FiltersDisplay.java
@@ -1,13 +1,29 @@
 package com.example.kevin.ebooklibrary;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class FiltersDisplay extends AppCompatActivity {
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent info = getIntent();
+        String query = info.getStringExtra("query");
+        db = SQLiteDatabase.openDatabase("EBookLib.db", null, 0);
+        Cursor data = db.rawQuery( query, null);
+        RecyclerView display = (RecyclerView) findViewById(R.id.filterList);
+        display.setLayoutManager(new LinearLayoutManager(this));
+        while (data.moveToNext()){
+
+        }
         setContentView(R.layout.activity_filters);
     }
 }
--- app/src/main/java/com/example/kevin/ebooklibrary/Insert.java
+++ app/src/main/java/com/example/kevin/ebooklibrary/Insert.java
@@ -1,13 +1,17 @@
 package com.example.kevin.ebooklibrary;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class Insert extends AppCompatActivity {
 
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = SQLiteDatabase.openDatabase("EBookLib.db", null, 0);
         setContentView(R.layout.activity_insert);
     }
 }
