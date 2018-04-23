fa6a33e4d5fb74e6c783e4c7947e031ea65620b2
Kminttech <kminttech@gmail.com>
2018-04-23 16:37:22 -0500

    Add Tag

--- app/src/main/java/com/example/kevin/ebooklibrary/Insert.java
+++ app/src/main/java/com/example/kevin/ebooklibrary/Insert.java
@@ -1,11 +1,13 @@
 package com.example.kevin.ebooklibrary;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,12 +18,28 @@ public class Insert extends AppCompatActivity {
     private SQLiteDatabase db;
     private String filePath;
     private String coverPath;
-    private ArrayList<String> tags;
+    private ArrayList<String> tagNames;
+    private ArrayList<Integer> tagIDs;
+    private EditText titleEntry;
+    private EditText firstNameEntry;
+    private EditText lastNameEntry;
+    private EditText tagEntry;
+    private TextView fileDisplay;
+    private TextView coverDisplay;
+    private TextView tagDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         db = SQLiteDatabase.openDatabase("EBookLib.db", null, 0);
+        tagIDs = new ArrayList<>();
+        titleEntry = findViewById(R.id.enterTitle);
+        firstNameEntry = findViewById(R.id.enterFirstName);
+        lastNameEntry = findViewById(R.id.enterLastName);
+        tagEntry = findViewById(R.id.newTagEntry);
+        fileDisplay = findViewById(R.id.filePathDisplay);
+        coverDisplay = findViewById(R.id.displayCoverPath);
+        tagDisplay = findViewById(R.id.tagListDisplay);
         setContentView(R.layout.activity_insert);
     }
 
@@ -49,21 +67,33 @@ public class Insert extends AppCompatActivity {
             case ACTIVITY_CHOOSE_FILE: {
                 if (resultCode == RESULT_OK){
                     filePath = data.getData().getPath();
+                    fileDisplay.setText(filePath);
                 }
             }
             case ACTIVITY_CHOOSE_COVER: {
                 if (requestCode == RESULT_OK){
                     coverPath = data.getData().getPath();
+                    coverDisplay.setText(coverPath);
                 }
             }
         }
     }
 
     public void addTag(View view) {
-
+        String[] req = new String[1];
+        req[0] = tagEntry.getText().toString();
+        Cursor tagCheck = db.rawQuery("SELECT TagID FROM Tag WHERE Descriptor = ?", req);
+        if(!tagCheck.moveToNext()){
+            db.execSQL("INSERT INTO Tag (Descriptor) VALUES (?)", req);
+            tagCheck = db.rawQuery("SELECT TagID FROM Tag WHERE Descriptor = ?", req);
+            tagCheck.moveToNext();
+        }
+        tagIDs.add(tagCheck.getInt(tagCheck.getColumnIndex("Descriptor")));
+        tagDisplay.setText(tagDisplay.getText() + " " + req[0]);
     }
 
     public void confirmEntry(View view) {
+
 
     }
 
